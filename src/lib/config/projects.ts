9003dd8a645bba7dfcc7ff53e5a59a30cc42bc6b
sevichecc <sevicheee@outlook.com>
2022-05-26 11:59:31 +0800

    project page init

--- src/lib/config/projects.ts
+++ src/lib/config/projects.ts
@@ -1,21 +1,61 @@
-export interface Project{
-  id: number,
-  name: string,
-  tech: string[],
-  category: string,
-  description: string,
-  img: string,
-  github?: string,
-  demo: string
+export type Project = {
+  id: string
+  name?: string
+  tags?: string[]
+  feature?: string
+  description?: string
+  img?: string
+  link?: string
 }
 
-export const projects: Project = {
-  id: 1,
-  name: '知乎专栏mock',
-  tech: ['Vue 3', 'Composition API'],
-  category: 'SPA应用',
-  description: '这是一个前后端分离的应用，模仿的是知乎专栏',
-  img: 'https://s2.loli.net/2022/05/16/FDWr4b3mGOgcd5K.png',
-  github: 'https://github.com/Sevichecc/zhihu-post-mock',
-  demo: 'https://zheye.seviche.cc'
-}
+export const projects: Project[] = [
+  {
+    id: 'zheye',
+    name: '者也专栏',
+    tags: ['Vue 3', 'Composition API', 'Vue Router', 'Vuex', 'Axios'],
+    feature: 'Vue3 + TypeScript',
+    description: '这是一个前后端分离的应用，模仿的是知乎专栏',
+    img: 'https://uneorange.oss-cn-guangzhou.aliyuncs.com/202205251801454.avif',
+    link: 'https://zheye.seviche.cc'
+  },
+  {
+    id: 'coach',
+    name: 'Find a Coach',
+    tags: ['Vue 3', 'Composition API'],
+    feature: 'Vue3',
+    description:
+      '既然如何， 问题的关键究竟为何？ 要想清楚，科学和人文谁更有意义，到底是一种怎么样的存在。 普列姆昌德曾经提到过，希望的灯一旦熄灭，生活刹那间变成了一片黑暗。这启发了我， 那么， 我认为， 总结的来说，',
+    img: 'https://uneorange.oss-cn-guangzhou.aliyuncs.com/202205251801454.avif',
+    link: 'https://zheye.seviche.cc'
+  },
+  {
+    id: 'fokify',
+    name: 'Forkify-食谱平台',
+    tags: ['MVM', 'Composition API'],
+    feature: 'JavaScript',
+    description:
+      '既然如何， 问题的关键究竟为何？ 要想清楚，科学和人文谁更有意义，到底是一种怎么样的存在。 普列姆昌德曾经提到过，希望的灯一旦熄灭，生活刹那间变成了一片黑暗。这启发了我， 那么， 我认为， 总结的来说，',
+    img: 'https://s2.loli.net/2022/05/16/FDWr4b3mGOgcd5K.png',
+    link: 'https://forkify.seviche.cc'
+  },
+  {
+    id: 'bankist',
+    name: 'Bankist',
+    tags: ['Lazy-loading'],
+    feature: 'JavaScript',
+    description:
+      '既然如何， 问题的关键究竟为何？ 要想清楚，科学和人文谁更有意义，到底是一种怎么样的存在。 普列姆昌德曾经提到过，希望的灯一旦熄灭，生活刹那间变成了一片黑暗。这启发了我， 那么， 我认为， 总结的来说，',
+    img: 'https://s2.loli.net/2022/05/16/FDWr4b3mGOgcd5K.png',
+    link: 'https://bankist.seviche.cc'
+  },
+  {
+    id: 'omnifood',
+    name: 'Omnifood',
+    tags: ['Lazy-loading'],
+    feature: 'JavaScript',
+    description:
+      '既然如何， 问题的关键究竟为何？ 要想清楚，科学和人文谁更有意义，到底是一种怎么样的存在。 普列姆昌德曾经提到过，希望的灯一旦熄灭，生活刹那间变成了一片黑暗。这启发了我， 那么， 我认为， 总结的来说，',
+    img: 'https://s2.loli.net/2022/05/16/FDWr4b3mGOgcd5K.png',
+    link: 'https://omnifood.seviche.cc'
+  }
+]
--- urara/projects/index.svelte
+++ urara/projects/index.svelte
@@ -1,7 +1,13 @@
 <script lang='ts'>
+import { projects as allProjects } from '$lib/config/projects'
 import Head from '$lib/components/head.svelte'
 import ProjectComponent from '$lib/components/extra/projects.svelte'
+
+let items: { id: string }[] = [...(allProjects as { id: string }[]), { id: 'footer' }]
 </script>
 
 <Head/>
-<ProjectComponent />
+
+{#each items as item}
+ <ProjectComponent {item}/>
+{/each}
