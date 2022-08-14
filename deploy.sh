984f9fda91239e3312e47f6edf91d080cbba3792
Sevichecc <91365763+Sevichecc@users.noreply.github.com>
2022-08-14 18:20:34 +0800

    fix format

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "1. build locally"
+echo "————————1. Build locally————————"
 netlify build 
-echo "2.deploy test"
+echo "————————2.Deploy test————————"
 netlify deploy
-echo "3.deploy"
+echo "————————3.Deploy————————"
 netlify deploy --prod
