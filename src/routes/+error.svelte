bd7d35d050c33716e7ab8a7423ff55695b509a15
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2022-09-10 11:18:21 +0800

    merge with upstream  4745cd

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,6 @@
   import { page } from '$app/stores'
   import Head from '$lib/components/head.svelte'
   import Footer from '$lib/components/footer.svelte'
-  console.error($page.status, $page.error.message)
 </script>
 
 <Head page={{ title: $page.status.toString() ?? '404', path: $page.url.pathname }} />
@@ -17,7 +16,7 @@
         <h1 class="opacity-20 text-6xl md:text-[12rem] -mt-2 mb-0">
           {$page.status ?? '404'}
         </h1>
-        <h2 class="-mt-12 md:-mt-24">{$page.error.message ?? 'Not found'}</h2>
+        <h2 class="-mt-12 md:-mt-24">{$page.error?.message ?? 'Not found'}</h2>
         <div class="card-actions">
           <a href="/" class="btn btn-neutral no-underline shadow-xl hover:shadow-2xl mt-8">
             <span class="i-heroicons-outline-home -ml-1 mr-2" />
