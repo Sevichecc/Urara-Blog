fcc51c609f13e4c7d3059ec2deb79e624678349f
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-01-26 00:05:50 +0800

    merge with upsteam https://github.com/importantimport/urara/commit/85d1b5d057adc30d6956f9c5679123c41417978f

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -1,7 +1,7 @@
 import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
-  protocol: 'https://',
+  protocol: import.meta.env.URARA_SITE_PROTOCOL ?? 'https://',
   domain: 'seviche.cc',
   title: 'Seviche.cc',
   subtitle: 'Tech / Code / Random Life',
