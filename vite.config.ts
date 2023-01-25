b604050d9f076ef84b8300f1ff1b94c60677a423
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-01-26 00:30:36 +0800

    merge with upsteam https://github.com/importantimport/urara/commit/7f6cc6bc09bd7bc951fbf765d78721ef57f626e8

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import UnoCSS from 'unocss/vite'
 import { presetTagify, presetIcons, extractorSvelte } from 'unocss'
 import { imagetools } from 'vite-imagetools'
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
-import { sveltekit } from '@sveltejs/kit/vite'
+import { sveltekit as SvelteKit } from '@sveltejs/kit/vite'
 // postcss & tailwindcss
 import TailwindCSS from 'tailwindcss'
 import tailwindConfig from './tailwind.config'
@@ -47,7 +47,7 @@ export default defineConfig({
       ]
     }),
     imagetools(),
-    sveltekit(),
+    SvelteKit(),
     SvelteKitPWA({
       registerType: 'autoUpdate',
       manifest: false,
