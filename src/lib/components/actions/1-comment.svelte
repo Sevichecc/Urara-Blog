ea843494dd6e671144f0a9efec6832fe3d18534a
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-01-30 17:28:52 +0800

    refactor: ♻️ weaken the action buttons

---
+++ src/lib/components/actions/1-comment.svelte
@@ -0,0 +1,5 @@
+<button class="tooltip tooltip-left opacity-60 hover:opacity-100" data-tip="Jump to comments">
+  <a href="#post-comment" class="btn btn-circle btn-ghost hover:bg-red-10">
+    <span class="i-heroicons-outline-chat-alt-2" />
+  </a>
+</button>
