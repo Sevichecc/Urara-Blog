1a32b7e9b888301152a1ae58dae568c396cddb7e
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-01-30 17:28:52 +0800

    refactor: ♻️ weaken the action buttons

--- src/lib/components/actions/2-translate.svelte
+++ src/lib/components/actions/2-translate.svelte
@@ -3,10 +3,12 @@
   export let post: Urara.Post
 </script>
 
-<a
-  href={`https://translate.google.com/translate?sl=auto&tl=${
-    navigator.languages ? navigator.languages[0] : navigator.language
-  }&u=${site.protocol + site.domain + post.path}`}
-  class="btn btn-lg btn-circle btn-ghost bg-base-100 shadow-lg hover:shadow-xl">
-  <span class="i-heroicons-outline-translate" />
-</a>
+<button class="tooltip tooltip-left opacity-60 hover:opacity-100" data-tip="Translate">
+  <a
+    href={`https://translate.google.com/translate?sl=auto&tl=${
+      navigator.languages ? navigator.languages[0] : navigator.language
+    }&u=${site.protocol + site.domain + post.path}`}
+    class="btn btn-circle btn-ghost">
+    <span class="i-heroicons-outline-translate" />
+  </a>
+</button>
