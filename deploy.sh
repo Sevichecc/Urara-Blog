254ccca9b16abc5ffc1e45fa681ac598bfa7ebd2
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-02-03 23:40:54 +0800

    refactor: ♻️ remove deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "————————1. Build locally————————"
 netlify build 
-echo "————————2.Deploy test———————————"
-netlify deploy
-echo "————————3.Deploy————————————————"
+echo "————————2.Deploy————————————————"
 netlify deploy --prod
